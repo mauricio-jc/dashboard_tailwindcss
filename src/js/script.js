6bf7829c003fdc741d57ce65a773def819ee1481
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2024-07-12 20:45:16 -0300

    Feito página de serviços e modal.

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
             dropdownContent.classList.toggle('show');
         });
     });
+
+    const openModalButtons = document.querySelectorAll('.open-modal');
+    const closeModalButtons = document.querySelectorAll('.close-modal');
+
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const modalId = event.currentTarget.getAttribute('data-modal-target');
+            const modal = document.getElementById(modalId);
+            modal.classList.remove('invisible');
+            setTimeout(() => modal.classList.add('opacity-100'), 10);
+        });
+    });
+
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const modal = event.currentTarget.closest('.fixed');
+            modal.classList.remove('opacity-100');
+            setTimeout(() => modal.classList.add('invisible'), 300);
+        });
+    });
 });
