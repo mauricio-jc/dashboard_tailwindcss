1e4147f07c2d8283e7cd3bc447000c40a667e9b4
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2024-07-12 10:10:58 -0300

    Ajustado dropdowns e criado tabela com dropdowns.

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,18 @@
-document.getElementById('dropdownNotification').addEventListener('click', function(event) {
-  event.preventDefault();
-  const dropdownMenuUser = document.getElementById('dropdownMenuNotification');
-  dropdownMenuUser.classList.toggle('opacity-0');
-  dropdownMenuUser.classList.toggle('invisible');
-  dropdownMenuUser.classList.toggle('opacity-100');
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.addEventListener('click', function(event) {
+        const dropdowns = document.querySelectorAll('.dropdown-content');
+        dropdowns.forEach(dropdown => {
+            if (!dropdown.parentElement.contains(event.target)) {
+                dropdown.classList.remove('show');
+            }
+        });
+    });
+    
+    document.querySelectorAll('.dropdown').forEach(anchor => {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault();
+            const dropdownContent = this.nextElementSibling;
+            dropdownContent.classList.toggle('show');
+        });
+    });
 });
-
-document.getElementById('dropdownUser').addEventListener('click', function(event) {
-  event.preventDefault();
-  const dropdownMenuUser = document.getElementById('dropdownMenuUser');
-  dropdownMenuUser.classList.toggle('opacity-0');
-  dropdownMenuUser.classList.toggle('invisible');
-  dropdownMenuUser.classList.toggle('opacity-100');
-});
-
-// Fechar o dropdown ao clicar fora dele
-document.addEventListener('click', function(event) {
-  var dropdownMenu = document.getElementById('dropdownMenuNotification');
-  var dropdownLink = document.getElementById('dropdownNotification');
-  if (!dropdownLink.contains(event.target) && !dropdownMenu.contains(event.target)) {
-    dropdownMenu.classList.add('opacity-0');
-    dropdownMenu.classList.add('invisible');
-    dropdownMenu.classList.remove('opacity-100');
-  }
-});
-
-document.addEventListener('click', function(event) {
-  var dropdownMenu = document.getElementById('dropdownMenuUser');
-  var dropdownLink = document.getElementById('dropdownUser');
-  if (!dropdownLink.contains(event.target) && !dropdownMenu.contains(event.target)) {
-    dropdownMenu.classList.add('opacity-0');
-    dropdownMenu.classList.add('invisible');
-    dropdownMenu.classList.remove('opacity-100');
-  }
-});
-
-// window.onclick = function(event) {
-//   if (!event.target.closest('.dropdown')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     for (var i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if(!openDropdown.classList.contains('opacity-0') && !openDropdown.classList.contains('invisible')) {
-//         openDropdown.classList.add('opacity-0');
-//         openDropdown.classList.add('invisible');
-//         openDropdown.classList.remove('opacity-100');
-//       }
-//     }
-//   }
-// }
